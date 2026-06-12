6686acdee399b5d7680c1c07bc58fb1d9989379c
Berton Quon <bertonquon@gmail.com>
2026-06-12 16:23:13 -0600

    Update icons

--- script.js
+++ script.js
@@ -58,7 +58,7 @@ function renderProjects() {
       </div>
       <div class="project-links">
         ${project.links.demo ? `<a href="${project.links.demo}" target="_blank">Demo</a>` : ''}
-        ${project.links.github ? `<a href="${project.links.github}" target="_blank">GitHub</a>` : ''}
+        ${project.links.github ? `<a href="${project.links.github}" target="_blank" class="project-icon-link" aria-label="Project GitHub" title="GitHub"><svg width="18" height="18" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="img"><path fill="currentColor" d="M12 .5C5.65.5.5 5.65.5 12c0 5.08 3.29 9.38 7.86 10.9.57.1.78-.25.78-.55 0-.27-.01-.98-.01-1.92-3.2.7-3.88-1.45-3.88-1.45-.52-1.32-1.27-1.67-1.27-1.67-1.04-.71.08-.7.08-.7 1.15.08 1.75 1.18 1.75 1.18 1.02 1.75 2.68 1.25 3.33.96.1-.75.4-1.25.73-1.54-2.55-.29-5.23-1.28-5.23-5.69 0-1.26.45-2.28 1.18-3.08-.12-.29-.51-1.46.11-3.04 0 0 .97-.31 3.18 1.18.92-.26 1.9-.39 2.87-.39.98 0 1.96.13 2.88.39 2.2-1.49 3.17-1.18 3.17-1.18.62 1.58.24 2.75.12 3.04.73.8 1.17 1.82 1.17 3.08 0 4.42-2.69 5.39-5.25 5.67.41.35.78 1.04.78 2.1 0 1.52-.01 2.75-.01 3.12 0 .3.2.66.79.55C20.71 21.38 24 17.08 24 12c0-6.35-5.15-11.5-12-11.5z"/></svg></a>` : ''}
         ${project.links.npm ? `<a href="${project.links.npm}" target="_blank">NPM</a>` : ''}
       </div>
     </div>
@@ -116,12 +116,16 @@ function renderContacts() {
   const { social } = portfolioData;
   const container = document.getElementById('contactLinks');
   
+  const githubSvg = `<img src="icons/github.svg" alt="GitHub" aria-hidden="true">`;
+  const linkedinSvg = `<img src="icons/linkedin.svg" alt="LinkedIn" aria-hidden="true">`;
+  const emailSvg = `<img src="icons/email.svg" alt="Email" aria-hidden="true">`;
+
   const links = [];
-  if (social.github) links.push(`<a href="${social.github}" target="_blank">GitHub</a>`);
-  if (social.linkedin) links.push(`<a href="${social.linkedin}" target="_blank">LinkedIn</a>`);
-  if (social.email) links.push(`<a href="mailto:${social.email}">Email</a>`);
-  if (social.twitter) links.push(`<a href="${social.twitter}" target="_blank">Twitter</a>`);
-  
+  if (social.github) links.push(`<a href="${social.github}" target="_blank" class="social-link" aria-label="GitHub" title="GitHub">${githubSvg}</a>`);
+  if (social.linkedin) links.push(`<a href="${social.linkedin}" target="_blank" class="social-link" aria-label="LinkedIn" title="LinkedIn">${linkedinSvg}</a>`);
+  if (social.email) links.push(`<a href="mailto:${social.email}" class="social-link" aria-label="Email" title="Email">${emailSvg}</a>`);
+  if (social.twitter) links.push(`<a href="${social.twitter}" target="_blank" class="social-link" aria-label="Twitter" title="Twitter">${social.twitter}</a>`);
+
   container.innerHTML = links.join('');
 }
 
